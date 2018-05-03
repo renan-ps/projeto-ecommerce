d43d3f21de12c4af02607463ad8292e4eec68e19
Renan Pereira Soares <renansoaresinfo@gmail.com>
2018-05-03 00:25:46 -0300

    Adiciona menu principal

---
+++ cabecalho.php
@@ -0,0 +1,54 @@
+<?php
+
+?>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-2 col-xs-12">
+				<img class="img-fluid" src="imagens/logo.jpg">
+			</div>
+			<div class="col-md-8 col-xs-12" style="margin-top: 4%" >
+				<input class="form-control" type="text" value="Buscar">
+			</div>
+			<div class="col-md-2 col-xs-12" style="margin-top: 4.5%; text-align: center;">
+				<p><a href="#">Login</a> ou <a href="#">Cadastre-se</a></p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+				<nav class="navbar navbar-expand-lg navbar-light bg-light">
+					<a class="navbar-brand" href="#">Navbar</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-	target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item active">
+								<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">Link</a>
+							</li>
+							<li class="nav-item dropdown">
+								<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									Dropdown
+								</a>
+								<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="#">Action</a>
+									<a class="dropdown-item" href="#">Another action</a>
+									<div class="dropdown-divider"></div>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</div>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link disabled" href="#">Disabled</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</div>
+		</div>
+			
+	</div>
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -1,34 +1,19 @@
-<?php
 
-?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
+<html>
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width-device-width, initial-scale=1 shrink-to-fit=no">
-		<title>Webster's </title>
+		<title>Webster's</title>
 		<link rel="stylesheet" href="css/bootstrap.min.css">
-
 	</head>
+
 	<body>
-		<div class="container">
-            <div class="row">
-                <div class="col-md-2 col-xs-12">
-                    <img class="img-fluid" src="imagens/logo.jpg">
-                </div>
-                <div class="col-md-8 col-xs-12" style="margin-top: 4%" >
-                    <input class="form-control" type="text" value="Buscar">
-                </div>
-                <div class="col-md-2 col-xs-12" style="margin-top: 4.5%">
-                    <p><a href="#">Login</a> ou <a href="#">Cadastre-se</a></p>
-                </div>
-                
-            </div>
-            
-            
-			
-		</div>
+		
+		<?php
+			include "cabecalho.php"
+		?>
+		
 		<script src="js/jquery-3.2.1.min.js"></script>
 		<script src="js/popper.min.js"></script>
 		<script src="js/bootstrap.min.js"></script>
